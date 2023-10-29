53d11ac308a3cb790c69d6425c784be5e8ef12cd
fernet_en_mano <91861841+skantos@users.noreply.github.com>
2023-10-28 22:48:05 -0300

    carga

--- src/components/loadingScreen.jsx
+++ src/components/loadingScreen.jsx
@@ -1,11 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './LoadingScreen.css';
 
 const LoadingScreen = () => {
+  const [textVisible, setTextVisible] = useState(false);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setTextVisible(true);
+    }, 500); 
+
+    return () => clearTimeout(timeout);
+  }, []);
+
   return (
     <div className="loading-screen">
-      <div className="loader"></div>
-      <p>Loading...</p>
+
+      {textVisible && (
+        <div className="text-background">
+          <p className="animated-text">
+            <span>H</span>
+            <span>a</span>
+            <span>m</span>
+            <span>s</span>
+            <span> </span>
+            <span>M</span>
+            <span>o</span>
+            <span>t</span>
+            <span>o</span>
+            <span>r</span>
+            <span>s</span>
+          </p>
+        </div>
+      )}
+
+      <div className="progress-bar">
+        <div className="progress-indicator"></div>
+      </div>
+      
     </div>
   );
 };
