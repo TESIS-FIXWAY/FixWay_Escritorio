2d27619535fc1194e366e4a03e48090c49b84ec0
Sebastián Quintana <101677885+sebaqqq@users.noreply.github.com>
2023-10-28 22:15:56 -0300

    loadingScreen

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 //componets (vistas)
 import Login from './components/login';
+import LoadingScreen from "./components/loadingScreen";
 //admin
 // agregar los nombres de las constantes en mayusculas
 import Admin from './components/admin/admin';
@@ -23,42 +24,53 @@ import { AuthContextProvider } from "./context/AuthContext";
 import ProtectedRoute from "./components/ProtectedRouter";
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+  React.useEffect(() => {
+    const fetchData = async () => {
+      await new Promise(resolve => setTimeout(resolve, 2000));
+      setIsLoading(false);
+    };
+    fetchData();
+  }, []);
+
   return (
       <div className="App">
-        <AuthContextProvider>
-          <Routes>
-            <Route path="/" element={<Login />} />
-            <Route
-              path='/admin'
-              element={
-                <ProtectedRoute>
-                  <Admin />
-                </ProtectedRoute>
-              }
-            />
-            <Route path='/indexAdmin' element={<IndexAdmin />} />
-            <Route path='/agregarUsuario' element={<AgregarUsuario />} />
-            <Route path='/listarUsuario' element={<ListarUsuario />} />
-            <Route path='/agregarFactura' element={<AgregarFactura />} />
-            <Route path='/listadoFacturas' element={<ListadoFacturas />} />
-            <Route path='/agregarInventario' element={<AgregarInventario />} />
-            <Route path='/listarInventario' element={<ListarInventario />} />
-            <Route path='/generarFactura' element={<GenerarFactura />} />
-            <Route
-              path='/mecanico'
-              element={
-                <ProtectedRoute>
-                  <Mecanico />
-                </ProtectedRoute>
-              }
-            />
-            <Route path="*" element={<Error />} />
-          </Routes>
-        </AuthContextProvider>
+        {isLoading ? (
+          <LoadingScreen />
+        ) : (
+          <AuthContextProvider>
+            <Routes>
+              <Route path="/" element={<Login />} />
+              <Route
+                path='/admin'
+                element={
+                  <ProtectedRoute>
+                    <Admin />
+                  </ProtectedRoute>
+                }
+              />
+              <Route path='/indexAdmin' element={<IndexAdmin />} />
+              <Route path='/agregarUsuario' element={<AgregarUsuario />} />
+              <Route path='/listarUsuario' element={<ListarUsuario />} />
+              <Route path='/agregarFactura' element={<AgregarFactura />} />
+              <Route path='/listadoFacturas' element={<ListadoFacturas />} />
+              <Route path='/agregarInventario' element={<AgregarInventario />} />
+              <Route path='/listarInventario' element={<ListarInventario />} />
+              <Route path='/generarFactura' element={<GenerarFactura />} />
+              <Route
+                path='/mecanico'
+                element={
+                  <ProtectedRoute>
+                    <Mecanico />
+                  </ProtectedRoute>
+                }
+              />
+              <Route path="*" element={<Error />} />
+            </Routes>
+          </AuthContextProvider>
+        )}
       </div>
   );
 }
-
-
 
 export default App;
