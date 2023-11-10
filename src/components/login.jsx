1ee55153cdf2fe00e7d3111e67f384cc20a06843
Cripster21 <103217341+Cripster21@users.noreply.github.com>
2023-11-10 19:34:26 -0300

    mensajito

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -8,6 +8,7 @@ import { useNavigate } from 'react-router-dom';
 const Login = () => {
   const [user, setUser] = useState(null);
   const navigate = useNavigate();
+  const [error, setError] = useState(null);
 
   async function getRol(uid) {
     const docuRef = doc(db, `users/${uid}`);
@@ -38,12 +39,18 @@ const Login = () => {
     }
   });
 
-  function handleSumit (e) {
+  async function handleSumit(e) {
+    e.preventDefault();
     const email = e.target.elements.email.value;
     const password = e.target.elements.password.value;
-    e.preventDefault();
-    signInWithEmailAndPassword(auth, email, password);
-    user.rol === 'administrador' ? navigate("/indexAdmin") : navigate("/mecanico");
+
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+      user.rol === 'administrador' ? navigate("/indexAdmin") : navigate("/mecanico");
+    } catch (error) {
+      console.error("Login failed", error.message);
+      setError("Correo o contraseña incorrectos.");
+    }
   }
 
   return (
@@ -53,35 +60,36 @@ const Login = () => {
           <div className="informacion">
               <div className="info">
                     <h2>Bienvenido</h2>
-                    <p>taller mecanico Hans Motors</p>
+                    <p>taller mecanico <br /> Hans Motors</p>
                 </div>
             </div>
             <div className="form-informacion">
                 <div className="form-info-childs">
-                    <h2>Iniciar sesion</h2>
-                    <form className="formulario" onSubmit={handleSumit} >
-                        <label className='label_login'>
-                            <i className='bx bx-envelope' ></i>
-                            <input 
-                              type="email" 
-                              placeholder="email"
-                              id='email'
-                            />
-                        </label>
-                        <label className='label_login'>
-                            <i className='bx bx-lock-alt'></i>                        
-                            <input
-                              type="password" 
-                              placeholder="contrasena"
-                              id='password'
-                            />
-                        </label>
-                        <input 
-                          type="submit" 
-                          value="Iniciar sesion"
-                          className="btn-enviar"
-                        />
-                    </form>
+                  <h2>Iniciar sesion</h2>
+                  <form className="formulario" onSubmit={handleSumit} >
+                      <label className='label_login'>
+                          <i className='bx bx-envelope' ></i>
+                          <input 
+                            type="email" 
+                            placeholder="email"
+                            id='email'
+                          />
+                      </label>
+                      <label className='label_login'>
+                          <i className='bx bx-lock-alt'></i>                        
+                          <input
+                            type="password" 
+                            placeholder="contrasena"
+                            id='password'
+                          />
+                      </label>
+                      <input 
+                        type="submit" 
+                        value="Iniciar sesion"
+                        className="btn-enviar"
+                      />
+                  </form>
+                  {error && <p style={{marginTop: 10, fontSize: 15}}>{error}</p>}
                 </div>
             </div>
           </div>
