586f227eb9aa0bb67c4214c1c8f2b0c33cfe3515
sebaqqq <sgquintana2002@gmail.com>
2023-11-18 16:45:56 -0300

    Update testing dependencies in package.json

---
+++ src/test/indexAdmin.test.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import { describe, it, expect } from 'vitest'
+import IndexAdmin from '../components/admin/indexAdmin';
+
+
+describe('IndexAdmin', () => {
+  it('should be a function', () => {
+    expect(typeof IndexAdmin).toBe('function')
+  })
+
+
+  it('should return a string', () => {
+    expect(typeof IndexAdmin()).toBe('string')
+  })
+});
+
