40477d1693d2c1b704509f7912fa318304038aa5
Sebastián Quintana <101677885+sebaqqq@users.noreply.github.com>
2024-04-29 22:39:24 -0400

    Material UI React

--- src/components/admin/agregarFactura.jsx
+++ src/components/admin/agregarFactura.jsx
@@ -1,31 +1,24 @@
-import React, { useState } from 'react';
-import { storage, db } from '../../firebase';
-import { 
-  collection, 
-  addDoc, 
-  serverTimestamp
-} from "firebase/firestore";
-import {
-  ref,
-  uploadBytesResumable,
-  getDownloadURL,
-} from "firebase/storage";
-import Admin from '../admin/admin';
+import React, { useState } from "react";
+import { storage, db } from "../../firebase";
+import { collection, addDoc, serverTimestamp } from "firebase/firestore";
+import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import Admin from "../admin/admin";
+import { Button } from "@mui/material";
 
 const AgregarFactura = () => {
   const [file, setFile] = useState(null);
   const [uploading, setUploading] = useState(false);
   const [mensaje, setMensaje] = useState(null);
   const [state, setState] = useState({
-    fecha: '',
-    proveedor: '',
-    detalle: '',
-    url: '',
+    fecha: "",
+    proveedor: "",
+    detalle: "",
+    url: "",
   });
 
   const handleChangeText = (name, value) => {
     setState({ ...state, [name]: value });
-  }
+  };
 
   const handleFileChange = (e) => {
     const selectedFile = e.target.files[0];
@@ -34,7 +27,7 @@ const AgregarFactura = () => {
 
   const handleUpload = async () => {
     if (!file) {
-      console.error('No se ha seleccionado ningún archivo.');
+      console.error("No se ha seleccionado ningún archivo.");
       return;
     }
     try {
@@ -43,22 +36,23 @@ const AgregarFactura = () => {
       const uploadTask = uploadBytesResumable(storageRef, file);
 
       uploadTask.on(
-        'state_changed',
+        "state_changed",
         (snapshot) => {
-          const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          const progress =
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
           console.log(`Progreso de la subida: ${progress}%`);
-          setMensaje('Se ha subido el archivo');
+          setMensaje("Se ha subido el archivo");
         },
         (error) => {
-          console.error('Error al subir el archivo:', error);
+          console.error("Error al subir el archivo:", error);
         },
         async () => {
           const downloadURL = await getDownloadURL(uploadTask.snapshot.ref);
-          console.log('URL del archivo subido:', downloadURL);
+          console.log("URL del archivo subido:", downloadURL);
         }
       );
     } catch (error) {
-      console.error('Error al subir el archivo:', error);
+      console.error("Error al subir el archivo:", error);
     } finally {
       setUploading(false);
     }
@@ -69,17 +63,17 @@ const AgregarFactura = () => {
       e.preventDefault();
       const { fecha, proveedor, detalle } = state;
       if (!fecha || !proveedor || !detalle) {
-        setMensaje('Datos incompletos');
+        setMensaje("Datos incompletos");
         return;
       }
 
       const timestampNow = serverTimestamp();
       await handleUpload();
       if (!file) {
-        setMensaje('Error al subir el archivo');
+        setMensaje("Error al subir el archivo");
         return;
       }
-      
+
       const storageRef = ref(storage, `facturas/${file.name}`);
       const downloadURL = await getDownloadURL(storageRef);
       const docRef = await addDoc(collection(db, "facturas"), {
@@ -89,86 +83,106 @@ const AgregarFactura = () => {
         timestamp: timestampNow,
         url: downloadURL,
       });
-  
+
       console.log("Documento escrito con ID:", docRef.id);
-      setMensaje('Se ha guardado correctamente');
+      setMensaje("Se ha guardado correctamente");
     } catch (error) {
       console.error("Error al agregar el documento:", error);
-      setMensaje('Ha ocurrido un error al guardar');
+      setMensaje("Ha ocurrido un error al guardar");
     } finally {
-      setState({ fecha: '', proveedor: '', detalle: '' });
+      setState({ fecha: "", proveedor: "", detalle: "" });
     }
-  }
-  
+  };
 
   return (
     <>
       <Admin />
-      <div >
-          <div className="body_formulario">
-            <div className='formulario_content'>
-              <div className='formulario_wrapper'>
-                <div className='formulario_contact'>
-                  <h1 className="formulario_titulo">Agregar Factura Proveedor</h1>
-                  <form className="formulario_form" onSubmit={handleSubmit}>
-                    <p>
-                      <label className='label_formulario'>Fecha</label>
-                      <br />
-                      <input
-                        id="fecha"
-                        type="date"
-                        name="fecha"
-                        placeholder="Fecha"
-                        className='input_formulario'
-                        required
-                        onChange={(e) => handleChangeText('fecha', e.target.value)}
-                        value={state.fecha}
-                      />
-                    </p>
-                    <p>
-                      <label className='label_formulario'>Proveedor</label>
-                      <br />
-                      <input
-                        id="proveedor"
-                        type="text"
-                        name="proveedor"
-                        placeholder="Proveedor"
-                        className='input_formulario'
-                        required
-                        onChange={(e) => handleChangeText('proveedor', e.target.value)}
-                        value={state.proveedor}
-                      />
-                    </p>
-                    <p>
-                      <label className='label_formulario'>Detalle</label>
-                      <br />
-                      <input
-                        id="detalle"
-                        required
-                        type="text"
-                        className='input_formulario'
-                        name="detalle"
-                        placeholder="Detalle"
-                        onChange={(e) => handleChangeText('detalle', e.target.value)}
-                        value={state.detalle}
-                      />
-                      </p>
-                      <p>
-                        <label className='label_formulario'>Seleccionar Archivo</label>
-                        <br />
-                        <input type="file" onChange={handleFileChange} required className='input_formulario'/>
-                      </p>
-                      {mensaje && <p className="mensaje">{mensaje}</p>}
-                      <p className="block_boton">
-                        <button type="submit" disabled={uploading} className='boton_formulario' >Agregar Fatura</button>
-                        {uploading && <p>Subiendo Archivo</p> }
-                      </p>
-                  </form>
-                </div>
+      <div>
+        <div className="body_formulario">
+          <div className="formulario_content">
+            <div className="formulario_wrapper">
+              <div className="formulario_contact">
+                <h1 className="formulario_titulo">Agregar Factura Proveedor</h1>
+                <form className="formulario_form" onSubmit={handleSubmit}>
+                  <p>
+                    <label className="label_formulario">Fecha</label>
+                    <br />
+                    <input
+                      id="fecha"
+                      type="date"
+                      name="fecha"
+                      placeholder="Fecha"
+                      className="input_formulario"
+                      required
+                      onChange={(e) =>
+                        handleChangeText("fecha", e.target.value)
+                      }
+                      value={state.fecha}
+                    />
+                  </p>
+                  <p>
+                    <label className="label_formulario">Proveedor</label>
+                    <br />
+                    <input
+                      id="proveedor"
+                      type="text"
+                      name="proveedor"
+                      placeholder="Proveedor"
+                      className="input_formulario"
+                      required
+                      onChange={(e) =>
+                        handleChangeText("proveedor", e.target.value)
+                      }
+                      value={state.proveedor}
+                    />
+                  </p>
+                  <p>
+                    <label className="label_formulario">Detalle</label>
+                    <br />
+                    <input
+                      id="detalle"
+                      required
+                      type="text"
+                      className="input_formulario"
+                      name="detalle"
+                      placeholder="Detalle"
+                      onChange={(e) =>
+                        handleChangeText("detalle", e.target.value)
+                      }
+                      value={state.detalle}
+                    />
+                  </p>
+                  <p>
+                    <label className="label_formulario">
+                      Seleccionar Archivo
+                    </label>
+                    <br />
+                    <input
+                      type="file"
+                      onChange={handleFileChange}
+                      required
+                      className="input_formulario"
+                    />
+                  </p>
+                  {mensaje && <p className="mensaje">{mensaje}</p>}
+                  <p className="block_boton">
+                    <Button
+                      variant="outlined"
+                      type="submit"
+                      size="large"
+                      disabled={uploading}
+                      style={{ with: "120px", fontSize: "20px" }}
+                    >
+                      Agregar Factura Proveedor
+                    </Button>
+                    {uploading && <p>Subiendo Archivo</p>}
+                  </p>
+                </form>
               </div>
             </div>
           </div>
-        </div> 
+        </div>
+      </div>
     </>
   );
 };
--- src/components/admin/agregarUsuario.jsx
+++ src/components/admin/agregarUsuario.jsx
@@ -1,22 +1,23 @@
-import '../styles/agregarUsuario.css';
-import { useState, useEffect } from 'react';
+import "../styles/agregarUsuario.css";
+import { useState, useEffect } from "react";
 import {
   signInWithEmailAndPassword,
   createUserWithEmailAndPassword,
   signOut,
-} from 'firebase/auth';
-import { 
-  doc, 
-  setDoc, 
+} from "firebase/auth";
+import {
+  doc,
+  setDoc,
   onSnapshot,
   collection,
   getDocs,
   query,
-  where
-} from 'firebase/firestore';
-import { db, auth } from '../../firebase'; 
-import Admin from './admin';
-import validadorRUT from './validadorRUT';
+  where,
+} from "firebase/firestore";
+import { db, auth } from "../../firebase";
+import Admin from "./admin";
+import validadorRUT from "./validadorRUT";
+import { Button } from "@mui/material";
 
 const AgregarUsuario = () => {
   const [mensaje, setMensaje] = useState(null);
@@ -28,7 +29,7 @@ const AgregarUsuario = () => {
 
   useEffect(() => {
     if (identifyUser) {
-      const userRef = doc(db, 'users', identifyUser.uid);
+      const userRef = doc(db, "users", identifyUser.uid);
       onSnapshot(userRef, (snapshot) => {
         setUser(snapshot.data());
       });
@@ -40,27 +41,27 @@ const AgregarUsuario = () => {
       await signOut(auth);
       setUser(null);
     } catch (error) {
-      console.error('Error during logout:', error);
+      console.error("Error during logout:", error);
     }
   };
 
   const checkDuplicateRut = async (rut) => {
-    const usersRef = collection(db, 'users');
-    const snapshot = await getDocs(query(usersRef, where('rut', '==', rut)));
+    const usersRef = collection(db, "users");
+    const snapshot = await getDocs(query(usersRef, where("rut", "==", rut)));
     return !snapshot.empty;
-  };  
+  };
 
   const submitHandler = async (e) => {
     e.preventDefault();
-  
+
     if (validarCampos()) {
       const rut = e.target.elements.rut.value;
       const isDuplicateRut = await checkDuplicateRut(rut);
       if (isDuplicateRut) {
-        setMensajeValidacion('El Rut ya está registrado en otro usuario');
+        setMensajeValidacion("El Rut ya está registrado en otro usuario");
         return;
       }
-  
+
       const rol = e.target.elements.rol.value;
       const nombre = e.target.elements.nombre.value;
       const apellido = e.target.elements.apellido.value;
@@ -70,13 +71,17 @@ const AgregarUsuario = () => {
       const password = e.target.elements.password.value;
       const salario = e.target.elements.salario.value;
       const fechaIngreso = e.target.elements.fechaIngreso.value;
-  
+
       try {
         const currentUser = auth.currentUser;
-        const userCredentials = await createUserWithEmailAndPassword(auth, email, password);
+        const userCredentials = await createUserWithEmailAndPassword(
+          auth,
+          email,
+          password
+        );
         const newUser = userCredentials.user;
-  
-        await setDoc(doc(db, 'users', newUser.uid), {
+
+        await setDoc(doc(db, "users", newUser.uid), {
           rut,
           rol,
           nombre,
@@ -87,16 +92,16 @@ const AgregarUsuario = () => {
           salario,
           fechaIngreso,
         });
-  
-        setMensaje('Usuario añadido Correctamente');
-  
+
+        setMensaje("Usuario añadido Correctamente");
+
         await logoutAndReauthenticate();
-  
+
         clearFormFields();
         setMensajeValidacion(null);
         setMensajeRut(null);
       } catch (error) {
-        console.error('Error during user registration:', error);
+        console.error("Error during user registration:", error);
       } finally {
         setTimeout(() => {
           setMensaje(null);
@@ -104,49 +109,49 @@ const AgregarUsuario = () => {
       }
     }
   };
-  
+
   const validarRutOnChange = () => {
-    const rut = document.getElementById('rut').value;
+    const rut = document.getElementById("rut").value;
     const validador = new validadorRUT(rut);
     if (validador.esValido) {
-      document.getElementById('rut').value = validador.formateado();
-      setMensajeRut('RUT válido');
+      document.getElementById("rut").value = validador.formateado();
+      setMensajeRut("RUT válido");
     } else {
-      setMensajeRut('RUT inválido');
+      setMensajeRut("RUT inválido");
     }
   };
 
   const validarCampos = () => {
-    if (mensajeRut !== 'RUT válido') {
-      setMensajeValidacion('El Rut no es válido');
+    if (mensajeRut !== "RUT válido") {
+      setMensajeValidacion("El Rut no es válido");
       return false;
     }
     return true;
   };
 
   const formatSalaryInput = (input) => {
-    const value = input.value.replace(/[^0-9]/g, '');
+    const value = input.value.replace(/[^0-9]/g, "");
     if (value.length > 0) {
-      input.value = parseInt(value).toLocaleString('es-CL');
+      input.value = parseInt(value).toLocaleString("es-CL");
     }
   };
 
   const clearFormFields = () => {
     const fieldIds = [
-      'rut',
-      'rol',
-      'nombre',
-      'apellido',
-      'telefono',
-      'direccion',
-      'salario',
-      'password',
-      'email',
-      'fechaIngreso',
+      "rut",
+      "rol",
+      "nombre",
+      "apellido",
+      "telefono",
+      "direccion",
+      "salario",
+      "password",
+      "email",
+      "fechaIngreso",
     ];
 
     fieldIds.forEach((fieldId) => {
-      document.getElementById(fieldId).value = '';
+      document.getElementById(fieldId).value = "";
     });
   };
 
@@ -154,8 +159,10 @@ const AgregarUsuario = () => {
     try {
       await signOut(auth);
 
-      const userEmail = prompt('Ingrese su correo electrónico para agregar el nuevo usuario:');
-      const userPassword = prompt('Ingrese su contraseña para confirmar:');
+      const userEmail = prompt(
+        "Ingrese su correo electrónico para agregar el nuevo usuario:"
+      );
+      const userPassword = prompt("Ingrese su contraseña para confirmar:");
 
       await signInWithEmailAndPassword(auth, userEmail, userPassword);
 
@@ -163,46 +170,46 @@ const AgregarUsuario = () => {
 
       const currentUser = auth.currentUser;
       if (currentUser) {
-        const userRef = doc(db, 'users', currentUser.uid);
+        const userRef = doc(db, "users", currentUser.uid);
         onSnapshot(userRef, (snapshot) => {
           setUser(snapshot.data());
         });
       }
 
-      console.log('Sesión cerrada y reiniciada correctamente');
+      console.log("Sesión cerrada y reiniciada correctamente");
     } catch (error) {
-      console.error('Error durante el cierre de sesión y reinicio:', error);
+      console.error("Error durante el cierre de sesión y reinicio:", error);
     }
   };
 
   return (
     <>
-      <Admin/>
-      <div className='body_formulario'>
-        <div className='formulario_content'>
-          <div className='formulario_wrapper'>
-            <div className='formulario_contact'>
-              <h1 className='formulario_titulo'>Agregar Usuario</h1>
-              <form className="formulario_form"onSubmit={submitHandler}>
+      <Admin />
+      <div className="body_formulario">
+        <div className="formulario_content">
+          <div className="formulario_wrapper">
+            <div className="formulario_contact">
+              <h1 className="formulario_titulo">Agregar Usuario</h1>
+              <form className="formulario_form" onSubmit={submitHandler}>
                 <p>
-                  <label className='label_formulario'>RUT</label>
+                  <label className="label_formulario">RUT</label>
                   <br />
                   <input
-                    className='input_formulario'
+                    className="input_formulario"
                     id="rut"
                     type="text"
                     name="rut"
                     placeholder="Rut (11.111.111-1)"
                     required
-                    onChange={validarRutOnChange}  // Utiliza onChange en lugar de onBlur
+                    onChange={validarRutOnChange}
                   />
-                  <p className='mensaje_rut'>{mensajeRut}</p>
+                  <p className="mensaje_rut">{mensajeRut}</p>
                 </p>
                 <p>
-                  <label className='label_formulario'>ROL</label>
+                  <label className="label_formulario">ROL</label>
                   <br />
                   <select
-                    className='input_formulario'
+                    className="input_formulario"
                     id="rol"
                     name="rol"
                     required
@@ -212,69 +219,73 @@ const AgregarUsuario = () => {
                   </select>
                 </p>
                 <p>
-                  <label className='label_formulario'>Nombre</label>                  
+                  <label className="label_formulario">Nombre</label>
                   <br />
                   <input
-                    className='input_formulario'
+                    className="input_formulario"
                     id="nombre"
                     required
                     type="text"
                     name="nombre"
-                    placeholder="Nombre"/>
+                    placeholder="Nombre"
+                  />
                 </p>
                 <p>
-                  <label className='label_formulario'>Apellido</label>
+                  <label className="label_formulario">Apellido</label>
                   <br />
                   <input
-                    className='input_formulario'
+                    className="input_formulario"
                     id="apellido"
                     required
                     type="text"
                     name="apellido"
-                    placeholder="Apellido"/>
+                    placeholder="Apellido"
+                  />
                 </p>
                 <p>
-                  <label className='label_formulario'>Teléfono</label>
+                  <label className="label_formulario">Teléfono</label>
                   <br />
                   <input
-                    className='input_formulario'
+                    className="input_formulario"
                     id="telefono"
                     required
                     type="tel"
                     name="telefono"
                     pattern="[+]56 [0-9]{1} [0-9]{8}"
-                    placeholder="Ejemplo: +56 9 12345678"/>
+                    placeholder="Ejemplo: +56 9 12345678"
+                  />
                 </p>
                 <p>
-                  <label className='label_formulario'>Dirección</label>
+                  <label className="label_formulario">Dirección</label>
                   <br />
                   <input
-                    className='input_formulario'
+                    className="input_formulario"
                     id="direccion"
                     required
                     type="text"
                     name="direccion"
-                    placeholder="Direccion"/>
-                </p>
-                <p>
-                  <label className='label_formulario'>Sueldo</label>
-                  <br />
-                  <input
-                  className='input_formulario'
-                  id="salario"
-                  required
-                  type="text" 
-                  name="salario"
-                  placeholder="Salario"
-                  pattern="[0-9.,]+"
-                  onChange={(e) => formatSalaryInput(e.target)}
+                    placeholder="Direccion"
                   />
                 </p>
                 <p>
-                  <label className='label_formulario'>Fecha de Ingreso</label>
+                  <label className="label_formulario">Sueldo</label>
                   <br />
                   <input
-                    className='input_formulario'
+                    className="input_formulario"
+                    id="salario"
+                    required
+                    type="text"
+                    name="salario"
+                    placeholder="Salario"
+                    pattern="[0-9.,]+"
+                    onChange={(e) => formatSalaryInput(e.target)}
+                  />
+                </p>
+                <p>
+                  <label className="label_formulario">Fecha de Ingreso</label>
+                  <br />
+                  <input
+                    className="input_formulario"
                     id="fechaIngreso"
                     required
                     type="date"
@@ -283,10 +294,10 @@ const AgregarUsuario = () => {
                   />
                 </p>
                 <p>
-                  <label className='label_formulario'>Email</label>
+                  <label className="label_formulario">Email</label>
                   <br />
                   <input
-                    className='input_formulario'
+                    className="input_formulario"
                     id="email"
                     required
                     type="text"
@@ -295,10 +306,10 @@ const AgregarUsuario = () => {
                   />
                 </p>
                 <p>
-                  <label className='label_formulario'>Contraseña</label>
+                  <label className="label_formulario">Contraseña</label>
                   <br />
                   <input
-                    className='input_formulario'
+                    className="input_formulario"
                     id="password"
                     required
                     type="text"
@@ -306,15 +317,17 @@ const AgregarUsuario = () => {
                     placeholder="Contraseña"
                   />
                 </p>
-                <p className='block_boton'>
-                <p className="mensaje">{mensaje}</p>
-                <p className='mensaje_validacion'>{mensajeValidacion}</p>
-                  <button 
-                    type="submit" 
-                    className='boton_formulario'
+                <p className="block_boton">
+                  <p className="mensaje">{mensaje}</p>
+                  <p className="mensaje_validacion">{mensajeValidacion}</p>
+                  <Button
+                    variant="outlined"
+                    type="submit"
+                    size="large"
+                    style={{ with: "120px", fontSize: "20px" }}
                   >
-                    Agregar
-                  </button>
+                    Agregar Usuario
+                  </Button>
                 </p>
               </form>
             </div>
