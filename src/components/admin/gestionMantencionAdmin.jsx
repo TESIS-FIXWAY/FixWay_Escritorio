ead9668874fd334ac4ffc2e59fb0914b10e5b079
sebaqqq <sgquintana2002@gmail.com>
2024-05-25 11:45:19 -0400

    cambio en gestion de manencion

--- src/components/admin/gestionMantencionAdmin.jsx
+++ src/components/admin/gestionMantencionAdmin.jsx
@@ -1,95 +1,70 @@
-import '../styles/gestionMantenciones.css'
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import Admin from "./admin";
-import { db } from '../../firebase';
-import {
-  collection,
-  onSnapshot,
-  query,
-  doc,
-  updateDoc,
-} from 'firebase/firestore';
+import { db } from "../../firebase";
+import { collection, onSnapshot, query, getDocs } from "firebase/firestore";
+import "../styles/gestionMantenciones.css";
 
 const GestionMantencionesAdmin = () => {
   const [beginTask, setBeginTask] = useState([]);
   const [inProgressTasks, setInProgressTasks] = useState([]);
   const [completedTasks, setCompletedTasks] = useState([]);
   const [expandedTask, setExpandedTask] = useState(null);
+  const [users, setUsers] = useState({});
 
   const calculateContainerHeight = (tasks) => {
-    return tasks.length === 0 ? '200px' : `${tasks.length * 40}px`;
-  };
-
-  const fetchData = async () => {
-    try {
-      const mantencionesCollection = collection(db, 'mantenciones');
-      const q = query(mantencionesCollection);
-
-      const unsubscribe = onSnapshot(q, (querySnapshot) => {
-        const allTasksData = [];
-
-        querySnapshot.forEach((doc) => {
-          const task = { id: doc.id, ...doc.data(), estado: doc.data().estado };
-          allTasksData.push(task);
-        });
-
-        const sortedTasks = {
-          'pendiente': [],
-          'en proceso': [],
-          'terminado': [],
-        };
-
-        allTasksData.forEach((task) => {
-          sortedTasks[task.estado].push(task);
-        });
-
-        setBeginTask(sortedTasks['pendiente']);
-        setInProgressTasks(sortedTasks['en proceso']);
-        setCompletedTasks(sortedTasks['terminado']);
-      });
-
-      return () => unsubscribe();
-    } catch (error) {
-      console.error('Error fetching data:', error);
-    }
+    return tasks.length === 0 ? "200px" : `${tasks.length * 40}px`;
   };
 
   useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const mantencionesCollection = collection(db, "mantenciones");
+        const q = query(mantencionesCollection);
+
+        const unsubscribeMantenciones = onSnapshot(q, (querySnapshot) => {
+          const allTasksData = [];
+
+          querySnapshot.forEach((doc) => {
+            const task = {
+              id: doc.id,
+              ...doc.data(),
+              estado: doc.data().estado,
+            };
+            allTasksData.push(task);
+          });
+
+          const sortedTasks = {
+            pendiente: [],
+            "en proceso": [],
+            terminado: [],
+          };
+
+          allTasksData.forEach((task) => {
+            sortedTasks[task.estado].push(task);
+          });
+
+          setBeginTask(sortedTasks["pendiente"]);
+          setInProgressTasks(sortedTasks["en proceso"]);
+          setCompletedTasks(sortedTasks["terminado"]);
+        });
+
+        const usersCollection = collection(db, "users");
+        const usersSnapshot = await getDocs(usersCollection);
+        const usersData = {};
+        usersSnapshot.forEach((doc) => {
+          usersData[doc.id] = doc.data();
+        });
+
+        setUsers(usersData);
+
+        return () => unsubscribeMantenciones();
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
     fetchData();
   }, []);
-
-  useEffect(() => {
-    fetchData();
-  }, [beginTask, inProgressTasks, completedTasks]);
-
-  const updateTaskStatus = async (task, newStatus) => {
-    const taskRef = doc(db, 'mantenciones', task.id);
-  
-    try {
-      await updateDoc(taskRef, { estado: newStatus });
-
-      switch (newStatus) {
-        case 'en proceso':
-          setBeginTask((prevTodoTasks) => prevTodoTasks.filter((t) => t.id !== task.id));
-          if (!inProgressTasks.some((t) => t.id === task.id)) {
-            setInProgressTasks((prevInProgressTasks) => [...prevInProgressTasks, task]);
-          }
-          break;
-        case 'entregados':
-          setInProgressTasks((prevInProgressTasks) =>
-            prevInProgressTasks.filter((t) => t.id !== task.id)
-          );
-          if (!completedTasks.some((t) => t.id === task.id)) {
-            setCompletedTasks((prevCompletedTasks) => [...prevCompletedTasks, task]);
-          }
-          break;
-        default:
-          break;
-      }
-    } catch (error) {
-      console.error('Error updating task status:', error);
-    }
-  };
 
   const handleTaskExpand = (taskId) => {
     setExpandedTask((prevExpandedTask) =>
@@ -97,71 +72,107 @@ const GestionMantencionesAdmin = () => {
     );
   };
 
+  const getUserName = (userId) => {
+    const user = users[userId];
+    return user ? `${user.nombre} ${user.apellido}` : "Desconocido";
+  };
+
+  const formatDate = (date) => {
+    const day = date.getDate().toString().padStart(2, "0");
+    const month = (date.getMonth() + 1).toString().padStart(2, "0");
+    const year = date.getFullYear().toString().slice(-2);
+
+    return `${day}/${month}/${year}`;
+  };
+
   return (
     <>
       <Admin />
       <div>
-        <div>
-          <div className="container_mantencion_titulo">
-            <h1>Gestión de Mantenciones</h1>
+        <div className="container_mantencion_titulo">
+          <h1>Gestión de Mantenciones</h1>
+        </div>
+        <div className="container_mantencion">
+          <div className="container_mantencion_tareas">
+            <div className="container_mantencion_tareas_titulos">
+              <h2>Tareas por hacer</h2>
+            </div>
+            <ul style={{ height: calculateContainerHeight(beginTask) }}>
+              {beginTask.map((task) => (
+                <div
+                  key={task.id}
+                  className={`task-container ${
+                    expandedTask === task.id ? "expanded" : ""
+                  }`}
+                  onClick={() => handleTaskExpand(task.id)}
+                >
+                  <li>Patente: {task.id}</li>
+                  <li>Fecha: {formatDate(new Date(task.fecha))}</li>
+                  <li>Descripción: {task.descripcion}</li>
+                  <li>Kilometro de Mantención: {task.kilometrajeMantencion}</li>
+                  <li>
+                    Producto:{" "}
+                    {task.productos.map((producto, index) => (
+                      <p> - {producto.nombreProducto}</p>
+                    ))}
+                  </li>
+                </div>
+              ))}
+            </ul>
           </div>
-          <div className="container_mantencion">
-            <div className="container_mantencion_tareas">
-              <div className="container_mantencion_tareas_titulos">
-                <h2>Tareas por hacer</h2>
-              </div>
-              <ul style={{ height: calculateContainerHeight(beginTask) }}>
-                {beginTask.map((task) => (
-                  <div
-                    key={`${task.id}-${task.estado}`}
-                    className={`task-container ${expandedTask === task.id ? 'expanded' : ''}`}
-                    onClick={() => handleTaskExpand(task.id)}
-                  >
-                    <li>
-                      {task.id} <br />
-                      {task.descripcion}
-                    </li>
-                  </div>
-                ))}
-              </ul>
+          <div className="container_mantencion_tareas">
+            <div className="container_mantencion_tareas_titulos">
+              <h2>Tareas en Proceso</h2>
             </div>
-            <div className="container_mantencion_tareas">
-              <div className="container_mantencion_tareas_titulos">
-                <h2>Tareas en proceso</h2>
-              </div>
-              <ul>
-                {inProgressTasks.map((task) => (
-                  <div
-                    key={`${task.id}-${task.estado}`}
-                    className={`task-container ${expandedTask === task.id ? 'expanded' : ''}`}
-                    onClick={() => handleTaskExpand(task.id)}
-                  >
-                    <li>
-                      {task.id} <br />
-                      {task.descripcion}
-                    </li>
-                  </div>
-                ))}
-              </ul>
+            <ul style={{ height: calculateContainerHeight(inProgressTasks) }}>
+              {inProgressTasks.map((task) => (
+                <div
+                  key={task.id}
+                  className={`task-container ${
+                    expandedTask === task.id ? "expanded" : ""
+                  }`}
+                  onClick={() => handleTaskExpand(task.id)}
+                >
+                  <li>Patente: {task.id}</li>
+                  <li>Fecha: {formatDate(new Date(task.fecha))}</li>
+                  <li>Descripción: {task.descripcion}</li>
+                  <li>Kilometro de Mantención: {task.kilometrajeMantencion}</li>
+                  <li>Persona a Cargo: {getUserName(task.personaTomadora)}</li>
+                  <li>
+                    Producto:{" "}
+                    {task.productos.map((producto, index) => (
+                      <p> - {producto.nombreProducto}</p>
+                    ))}
+                  </li>
+                </div>
+              ))}
+            </ul>
+          </div>
+          <div className="container_mantencion_tareas">
+            <div className="container_mantencion_tareas_titulos">
+              <h2>Tareas Entregadas</h2>
             </div>
-            <div className="container_mantencion_tareas">
-              <div className="container_mantencion_tareas_titulos">
-                <h2>Entregados</h2>
-              </div>
-              <ul>
-                {completedTasks.map((task) => (
-                  <div
-                    key={`${task.id}-${task.estado}`}
-                    className={`task-container ${expandedTask === task.id ? 'expanded' : ''}`}
-                  >
-                    <li>
-                      {task.id} <br />
-                      {task.descripcion}
-                    </li>
-                  </div>
-                ))}
-              </ul>
-            </div>
+            <ul style={{ height: calculateContainerHeight(completedTasks) }}>
+              {completedTasks.map((task) => (
+                <div
+                  key={task.id}
+                  className={`task-container ${
+                    expandedTask === task.id ? "expanded" : ""
+                  }`}
+                >
+                  <li>Patente: {task.id}</li>
+                  <li>Fecha: {formatDate(new Date(task.fecha))}</li>
+                  <li>Descripción: {task.descripcion}</li>
+                  <li>Kilometro de Mantención: {task.kilometrajeMantencion}</li>
+                  <li>
+                    Producto:{" "}
+                    {task.productos.map((producto, index) => (
+                      <p> - {producto.nombreProducto}</p>
+                    ))}
+                  </li>
+                </div>
+              ))}
+            </ul>
           </div>
         </div>
       </div>
