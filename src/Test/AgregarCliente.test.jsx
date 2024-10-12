c5b583e54af1f8cb0513e35759d501d4be45cf01
sebaqqq <sgquintana2002@gmail.com>
2024-10-12 12:07:43 -0300

    test agregar cliente,  se necesita un npm install

---
+++ src/Test/AgregarCliente.test.jsx
@@ -0,0 +1,51 @@
+import { render, screen } from "@testing-library/react";
+import { describe, it, expect, vi } from "vitest";
+import React from "react";
+import AgregarCliente from "../components/admin/agregarCliente";
+import { MemoryRouter } from "react-router-dom";
+import { DarkModeContext } from "../context/darkMode";
+
+vi.mock("firebase/messaging", () => ({
+  getMessaging: () => ({}),
+  getToken: () => Promise.resolve("fake-token"),
+  onMessage: () => {},
+}));
+
+vi.mock("../context/AuthContext", () => ({
+  UserAuth: () => ({
+    user: { uid: "12345", nombre: "Test User" },
+    logout: vi.fn(),
+  }),
+}));
+
+describe("AgregarCliente", () => {
+  it("should render the client form correctly", () => {
+    const mockDarkModeContextValue = {
+      isDarkMode: false,
+      toggleDarkMode: () => {},
+    };
+
+    render(
+      <MemoryRouter>
+        <DarkModeContext.Provider value={mockDarkModeContextValue}>
+          <AgregarCliente />
+        </DarkModeContext.Provider>
+      </MemoryRouter>
+    );
+
+    const titleElement = screen.getByRole("heading", {
+      name: /Agregar Cliente/i,
+    });
+    expect(titleElement).not.toBeNull();
+
+    const nombreInput = screen.getByLabelText(/Nombre/i);
+    const apellidoInput = screen.getByLabelText(/Apellido/i);
+    expect(nombreInput).not.toBeNull();
+    expect(apellidoInput).not.toBeNull();
+
+    const submitButton = screen.getByRole("button", {
+      name: /Agregar Cliente/i,
+    });
+    expect(submitButton).not.toBeNull();
+  });
+});
--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,27 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { visualizer } from 'rollup-plugin-visualizer';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { visualizer } from "rollup-plugin-visualizer";
 
 export default defineConfig({
   plugins: [react(), visualizer()],
+  test: {
+    globals: true,
+    environment: "happy-dom",
+  },
   build: {
     rollupOptions: {
       output: {
         manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          if (id.includes("node_modules")) {
+            return id
+              .toString()
+              .split("node_modules/")[1]
+              .split("/")[0]
+              .toString();
           }
         },
       },
     },
-    chunkSizeWarningLimit: 1000, // Ajusta este valor según sea necesario
+    chunkSizeWarningLimit: 1000,
   },
 });
